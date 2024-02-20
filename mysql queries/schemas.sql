8019200fbb27d393b00bcf4c161a1151343d07a0
abhi <abhi@gma.com>
2024-02-20 15:38:42 +0530

    commit

--- mysql queries/schemas.sql
+++ mysql queries/schemas.sql
@@ -1,15 +1,17 @@
-create database events;
-
+drop database event;
+create database event;
+use event;
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root123';
 
 flush privileges;
 
+drop table events;
 CREATE TABLE events (
     id int auto_increment primary key,
     title varchar(200) not null,
-    description text not null,
-    startTime datetime not null,
-    endTime datetime not null,
+   --  description text not null,
+    start datetime not null,
+    end datetime not null,
     created_at timestamp default current_timestamp,
     updated_at timestamp
 );
